78eb090c63adebaabb08f4efd7a2fd5eb8f44f8c
Huynhhuynh <conghieuhuynh91@gmail.com>
2020-11-11 19:56:35 +0700

    Update quote form

--- inc/quote-form-shortcode.php
+++ inc/quote-form-shortcode.php
@@ -5,12 +5,18 @@
 
 function quote_form_shortcode_func( $atts ) {
 	$a = shortcode_atts( [
+    'heading' => __( 'Request a quote' ),
+    'description' => __( 'Please fill in your details below and a member of our Account Team will be in touch to discuss your requirements.' ),
     'className' => '',
   ], $atts );
 
   ob_start();
   ?>
-  <div class="<?= implode( ' ', [ 'quote-form-container', $a[ 'className' ] ] ) ?>"></div>
+  <div class="<?= implode( ' ', [ 'quote-form', $a[ 'className' ] ] ) ?>">
+    <h4 class="heading-text"><?= $a[ 'heading' ] ?></h4>
+    <?= wpautop( $a[ 'description' ] ) ?>
+    <div class="quote-form-container"></div>
+  </div>
   <?php 
   return ob_get_clean();
 }
--- src/app.js
+++ src/app.js
@@ -6,38 +6,47 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 
 const QuoteForm = () => {
-  const { register, handleSubmit, errors, watch } = useForm()
+  const { register, handleSubmit, errors, reset } = useForm()
   const onSubmit = data => {
-    alert( JSON.stringify( data ) )
+    /**
+     * Save !!!
+     */
+    alert( `Company Name: ${ data.companyname } / Phone Number: ${ data.phonenumber }` )
+    reset()
   }
-  console.log( watch( 'companyname' ) )
+
   return (
     <div className="quote-form-inner">
         <form className="form request-quote-form" onSubmit={ handleSubmit( onSubmit ) }>
           <label>
             <span className="__is-label">Company Name</span>
-            <input 
-              type="text" 
-              name="companyname" 
-              placeholder="..." 
-              ref={ register( {
-                required: true,
-              } ) }
-            />
-            { errors.companyname && <p style={ { color: 'red' } }>This field is required</p> }
+            <div className="field">
+              <input 
+                type="text" 
+                name="companyname" 
+                placeholder="..." 
+                ref={ register( {
+                  required: true,
+                } ) }
+              />
+              { errors.companyname && <p style={ { color: 'red' } }>This field is required</p> }
+            </div>
           </label>
           <label>
             <span class="__is-label">Phone Number</span>
-            <input 
-              type="text" 
-              name="phonenumber" 
-              placeholder="..." 
-              ref={ register( {
-                required: true
-              } ) }
-            />
-            { errors.phonenumber && <p style={ { color: 'red' } }>This field is required</p> }
+            <div className="field">
+              <input 
+                type="text" 
+                name="phonenumber" 
+                placeholder="..." 
+                ref={ register( {
+                  required: true
+                } ) }
+              />
+              { errors.phonenumber && <p style={ { color: 'red' } }>This field is required</p> }
+            </div>
           </label>
+          <div className="message-privacy-policy">The infomation collected will be used in accordance with our <a href="#">privacy policy</a>.</div>
           <input type="submit" className="button" value="Request a quote" />
         </form>
       </div>
